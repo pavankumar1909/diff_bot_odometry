3f0558d7e1c452cf6b84b76bdf9093464072315e
pavankumar1909 <pavankumarangajala09@gmai.com>
2026-02-06 15:37:26 +0530

    lidar integrated to ros2,saftey working with it

--- src/amr_bringup/launch/obstacle_avoidance.launch.py
+++ src/amr_bringup/launch/obstacle_avoidance.launch.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
@@ -37,6 +38,34 @@ def generate_launch_description():
         }.items()
     )
 
+    
+    # ✅ YAML file path
+    rplidar_params = os.path.join(
+        get_package_share_directory("amr_bringup"),
+        "config",
+        "rplidar_a1.yaml"
+    )
+
+    # ✅ Correct Node definition
+    rplidar_node = Node(
+    package="rplidar_ros",
+    executable="rplidar_composition",
+    name="rplidar_node",
+    output="screen",
+    parameters=[{
+            'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+            'frame_id': 'laser_link',
+            'angle_compensate': True,
+            'scan_mode': 'Standard',
+        }]
+    )
+#   {
+#      'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+#      'frame_id': 'laser_link',
+#      'angle_compensate': True,
+#      'scan_mode': 'Standard'
+#         }
+
     avoid_obstacles_node = Node(
         package="amr_utils",
         executable="avoid_obstacles.py"    
@@ -46,5 +75,6 @@ def generate_launch_description():
         hardware_interface,
         controller,
         joystick,
+        rplidar_node,
         avoid_obstacles_node
     ])
